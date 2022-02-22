2966f722e86415ad4321d0301f4437466f2dc699
sanjeet kumar <60851268+sanjeetkumargithub@users.noreply.github.com>
2022-02-22 08:22:20 +0530

    Delete Backend_code/quizPortal (3)/quizPortal directory

--- Backend_code/quizPortal (3)/quizPortal/quizPortal/src/main/java/in/ongrid/quizPortal/dao/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package in.ongrid.quizPortal.dao;
-import in.ongrid.quizPortal.entities.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface QuestionDao extends JpaRepository<Question,Integer> {
-    List<Question> findAllByQuizId(int quizid);
-}
--- Backend_code/quizPortal (3)/quizPortal/quizPortal/src/main/java/in/ongrid/quizPortal/dao/QuizDao.java
+++
@@ -1,8 +0,0 @@
-package in.ongrid.quizPortal.dao;
-
-import in.ongrid.quizPortal.entities.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizDao  extends JpaRepository<Quiz,Integer> {
-
-}
